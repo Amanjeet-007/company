7c89d3848d789f8d7cd10c62fefd2778dac8567d
Amanjeet <kumaramanjeet951@gmail.com>
2026-07-09 18:06:26 +0530

    Home page

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // dark mood  (theme options)
 const themeToggleBtn = document.getElementById('theme-toggle'); 
 
-// 1. पहले से सेव की गई थीम या सिस्टम प्रेफरेंस चेक करें
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark');
 } else {
     document.documentElement.classList.remove('dark');
 }
 
-// 2. बटन क्लिक होने पर थीम बदलें
 themeToggleBtn.addEventListener('click', function() {
     if (document.documentElement.classList.contains('dark')) {
         document.documentElement.classList.remove('dark');
